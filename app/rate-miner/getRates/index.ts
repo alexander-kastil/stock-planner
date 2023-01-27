c790ccaa6f09576e315949e0202a713d4ddbbeb8
Alexander Pajer <alexander.pajer@integrations.at>
2023-01-27 14:25:05 +0100

    rate fetch working

--- app/rate-miner/getRates/index.ts
+++ app/rate-miner/getRates/index.ts
@@ -1,6 +1,7 @@
 import { AzureFunction, Context } from '@azure/functions';
 import puppeteer from 'puppeteer';
 import { RateQuery } from '../model/rateQuery';
+import DomParser = require('dom-parser');
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     var timeStamp = new Date().toISOString();
@@ -14,8 +15,23 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     for (const query of queries) {
         await page.goto(query.url);
         const content = await page.content();
+        let parser = new DomParser();
+        const html = parser.parseFromString(content);
+        let ratesResult = html.getElementsByClassName('aktien-big-font');
+        if (ratesResult.length > 0) {
+            query.current = getNumber(ratesResult[0].textContent);
+            query.deltaCurr = getNumber(ratesResult[1].textContent);
+            query.deltaPercent = getNumber(ratesResult[2].textContent);
+            console.log(query);
+        }
     }
     await browser.close();
 };
+
+function getNumber(expr: string) {
+    const NUMERIC_REGEXP = /[-]{0,1}[\d]*[.]{0,1}[\d]+/g;
+    let num = expr.match(NUMERIC_REGEXP);
+    return +(num[0] + '.' + num[1]);
+}
 
 export default timerTrigger;
