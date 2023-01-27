e666863745600bd376acf8362a0a547aebcd757a
Alexander Pajer <alexander.pajer@integrations.at>
2023-01-27 20:36:42 +0100

    rate to queue

--- app/rate-miner/writeRatesToDB/index.ts
+++
@@ -1,7 +0,0 @@
-import { AzureFunction, Context } from '@azure/functions';
-
-const queueTrigger: AzureFunction = async function (context: Context, RateItem: string): Promise<void> {
-    context.log('Queue trigger function processed work item', RateItem);
-};
-
-export default queueTrigger;
